954fe2c302d5c39ee26959010768dba7eb46d369
Spencer Watson <spencer@wattydev.com>
2014-12-21 19:06:20 -0800

    Create contact page

--- js/controllers/contact.js
+++ js/controllers/contact.js
@@ -1,2 +1,34 @@
 app.controller('ContactController', ['$scope', '$rootScope', function(scope, root) {
+	//FUNCTIONS
+	scope.send = function() {
+		var content = {
+			email: scope.email,
+			name: scope.name,
+			subject: scope.subject,
+			comments: scope.comments,
+			to: 'spencer@wattydev.com'
+		};
+		$.ajax({
+			type: 'POST',
+			url: 'php/email.php',
+			data: content,
+			dataType: 'json',
+			success: function(data) {
+				if (data.sent) {
+					root.success('Your message has been sent');
+					scope.comments = '';
+					scope.$apply();
+					console.log('success');
+				} else {
+					root.error(data.message);
+					root.$apply();
+				}
+			},
+			error: function(data) {
+				var message = data.message || 'Unable to send message. Try again later';
+				root.error(message);
+				console.log('error');
+			}
+		});
+	};
 }]);
---
+++ php/email.php
@@ -0,0 +1,66 @@
+<?php
+require_once "header.php"; 
+
+$response = (object) array(
+    'sent' => true,
+    'message' => ''
+);
+
+//variables must exist
+if (!$_POST['email']) {
+	$response->sent = false;
+	$response->message = 'Please add your email address.';
+	echo json_encode($response);
+	return;
+}
+if (!$_POST['name']) {
+	$response->sent = false;
+	$response->message = 'Please add your name.';
+	echo json_encode($response);
+	return;
+}
+if (!$_POST['subject']) {
+	$response->sent = false;
+	$response->message = 'Please add a subject.';
+	echo json_encode($response);
+	return;
+}
+if (!$_POST['comments']) {
+	$response->sent = false;
+	$response->message = 'Please add your comments.';
+	echo json_encode($response);
+	return;
+}
+if (!$_POST['to']) {
+	$response->sent = false;
+	$response->message = 'Please add the to email address.';
+	echo json_encode($response);
+	return;
+}
+
+//emails must be valid
+if (!valid_email($_POST['email'])) {
+	$response->sent = false;
+	$response->message = 'Please enter a valid email address.';
+	echo json_encode($response);
+	return;
+}
+if (!valid_email($_POST['to'])) {
+	$response->sent = false;
+	$response->message = 'Please enter a valid to email address.';
+	echo json_encode($response);
+	return;
+}
+
+$message = $_POST['comments'] . "\n From: " . $_POST['name'];
+$subject = '[KitePaint] ' . $_POST['subject'];
+
+if (sendMail($_POST['to'], $subject, $message, $_POST['email'])) {
+	echo json_encode($response);
+	return;
+} else {
+	$response->sent = false;
+	$response->message = 'We were unable to send your message. Try again later.';
+	echo json_encode($response);
+	return;
+}
