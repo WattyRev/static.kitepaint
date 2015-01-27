2059094b38bb5e517f8b0e15e0049689dc1a7de5
Spencer Watson <spencer@wattydev.com>
2015-01-27 10:41:16 -0800

    Set up JS joining and minification

--- build.php
+++ build.php
@@ -1,10 +1,20 @@
 <?php
+require "JSMin.php";
 
 process_js();
 
 function process_js() {
 	$destination = fopen("js/app.min.js", "w") or die("Unable to open file!");
-	$content = 'test';
+
+	$files = json_decode(file_get_contents("dependencies.json"), true)["process"]["js"];
+
+	$content = "";
+	foreach ($files as $file) {
+		$content .= file_get_contents("js/".$file) . "\n";
+	}
+
+	$content = JSMIN::minify($content);
+	
 	fwrite($destination, $content);
-	fclose($myfile);
+	fclose($destination);
 }
