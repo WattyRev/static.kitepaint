cbeb23483a3acbd91a159055d528899d33e2e5b3
Spencer Watson <spencer@wattydev.com>
2015-01-31 09:19:49 -0800

    Fix base url reference in production

--- js/controllers/primary.js
+++ js/controllers/primary.js
@@ -2,7 +2,7 @@ app.controller('PrimaryController', ['$scope', '$rootScope', '$state', '$locatio
 
 	//VARIABLES
 		scope.current_page = '';
-		root.base_url = 'http://www.wattydev.com/projects/kite_paint';
+		root.base_url = environment === 'development' ? 'http://wattydev.com/projects/kite_paint' : 'http://kitepaint.com';
 		root.has_account = localStorage.kp_has_account ? localStorage.kp_has_account === 'true' : false;
 		root.sign_in = {
 			username: '',
