552936f3a751d67bc54d5b3c513cda8a93e19e50
Spencer Watson <wattyrev@gmail.com>
2019-01-01 09:15:52 -0800

    add test for reset

--- src/containers/__unit__/EditorContainer.test.js
+++ src/containers/__unit__/EditorContainer.test.js
@@ -546,4 +546,76 @@ describe("EditorContainer", () => {
       });
     });
   });
+  describe("reset", () => {
+    let wrapper;
+    beforeEach(() => {
+      defaultProps.product.variations = [
+        {
+          name: "Standard",
+          svg: ""
+        },
+        {
+          name: "Vented",
+          svg: ""
+        }
+      ];
+      wrapper = mount(
+        <EditorContainer {...defaultProps}>
+          {data => (
+            <React.Fragment>
+              <div className="output">
+                {JSON.stringify(data.props.appliedColors)}
+              </div>
+              <div className="reset" onClick={data.actions.reset} />
+            </React.Fragment>
+          )}
+        </EditorContainer>
+      );
+      wrapper.instance().setState({
+        appliedColors: {
+          Standard: {
+            p1: {
+              name: "black",
+              color: "#000000"
+            },
+            p2: {
+              name: "red",
+              color: "#ff0000"
+            },
+            p3: {
+              name: "green",
+              color: "#00ff00"
+            }
+          },
+          Vented: {
+            p1: {
+              name: "red",
+              color: "#ff0000"
+            },
+            p2: {
+              name: "black",
+              color: "#000000"
+            }
+          }
+        }
+      });
+    });
+    it("resets the current variation", () => {
+      expect.assertions(1);
+      wrapper.find(".reset").simulate("click");
+      expect(JSON.parse(wrapper.find(".output").text())).toEqual({
+        Standard: {},
+        Vented: {
+          p1: {
+            name: "red",
+            color: "#ff0000"
+          },
+          p2: {
+            name: "black",
+            color: "#000000"
+          }
+        }
+      });
+    });
+  });
 });
