5c29e2d49923f2cc89e5dbd813745b95713c1147
Spencer Watson <wattyrev@gmail.com>
2019-02-24 10:02:12 -0800

    Fix default coloring and add nodelist foreach polyfill

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import AppContainer from "./containers/AppContainer";
-import Store from "./redux";
 import { Provider as ReduxProvider } from "react-redux";
-
 import "normalize.css";
+import AppContainer from "./containers/AppContainer";
+import polyfill from "./utils/polyfill";
+import Store from "./redux";
+import "./index.css";
+
+polyfill();
 
 ReactDOM.render(
   <ReduxProvider store={Store}>
