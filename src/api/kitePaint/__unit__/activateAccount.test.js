12c971708baee1ca8a520e5dbf848968db9ab564
Spencer Watson <wattyrev@gmail.com>
2024-07-25 14:21:15 -0700

    Upgrade node (try again) (#191)
    * Remove gruntfile
    
    * Upgrade to node 15
    
    * Update workflows with ndoe 15
    
    * udpdate build version
    
    * Use Node 16
    
    * Resolve peer dependencies
    
    * Specify node version in workflows
    
    * audit fix
    
    * Fix registry
    
    * more package-lock fixes
    
    * workflow tweak
    
    * Update to node 18
    
    * workflow tweak
    
    * Fix build
    
    * Fix tests
    
    * Upgrade to node 20
    
    * Tweak workflow

--- src/api/kitePaint/__unit__/activateAccount.test.js
+++ src/api/kitePaint/__unit__/activateAccount.test.js
@@ -5,23 +5,28 @@ describe("#activateAccount", () => {
   beforeEach(() => {
     Api = new KitePaintApi();
     Api.axiosInstance = {
-      post: jest.fn().mockResolvedValue({}),
+      post: jest.fn().mockResolvedValue({
+        data: {
+          activated: true
+        }
+      }),
       get: jest.fn().mockResolvedValue({})
     };
   });
-  it("should send the procided data to the correct API", () => {
+  it("should send the procided data to the correct API", async () => {
     expect.assertions(2);
-    Api.activateAccount("abc", "def");
+    await Api.activateAccount("abc", "def");
     expect(Api.axiosInstance.post).toHaveBeenCalled();
     expect(Api.axiosInstance.post.mock.calls[0][0]).toEqual("/activate.php");
   });
-  it("should reject if the API returns no data", () => {
+  it("should reject if the API returns no data", async () => {
     expect.assertions(1);
-    return Api.activateAccount("abc", "def").catch(message => {
-      expect(message).toEqual("Could not activate the account");
-    });
+    Api.axiosInstance.post.mockResolvedValue({});
+    await expect(() => Api.activateAccount("abc", "def")).rejects.toThrow(
+      "Could not activate the account"
+    );
   });
-  it("should reject if the API returns that the activation was unsuccessful", () => {
+  it("should reject if the API returns that the activation was unsuccessful", async () => {
     expect.assertions(1);
     Api.axiosInstance.post.mockResolvedValue({
       data: {
@@ -29,11 +34,11 @@ describe("#activateAccount", () => {
         message: "it dont work"
       }
     });
-    return Api.activateAccount("abc", "def").catch(message => {
-      expect(message).toEqual("it dont work");
-    });
+    await expect(() => Api.activateAccount("abc", "def")).rejects.toThrow(
+      "it dont work"
+    );
   });
-  it("should reject if the API call fails", () => {
+  it("should reject if the API call fails", async () => {
     expect.assertions(1);
     Api.axiosInstance.post.mockRejectedValue("boogers");
     return Api.activateAccount("abc", "def").catch(message => {
