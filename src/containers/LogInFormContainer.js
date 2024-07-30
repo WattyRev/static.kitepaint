f3036b71943a4499f1d3efa830be31313a6d300c
Spencer Watson <wattyrev@gmail.com>
2024-07-29 19:27:36 -0700

    Update LogInFormContainer

--- src/containers/LogInFormContainer.js
+++ src/containers/LogInFormContainer.js
@@ -1,133 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import LogInForm from "../components/LogInForm";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import makeCancelable from "../utils/makeCancelable";
 import { LOG_IN } from "../redux/actions";
 
-export class LogInFormContainer extends React.Component {
-  static propTypes = {
-    /**
-     * An identifieng string used to differentiate this from other instances
-     */
-    id: PropTypes.string.isRequired,
-    /**
-     * A function called when the log in form is submitted.
-     * Provided by redux.
-     */
-    onSubmit: PropTypes.func.isRequired,
-    /**
-     * A function called when the link to the register form is clicked.
-     */
-    onRegister: PropTypes.func.isRequired,
-    /** A function called when the use successfully logs in */
-    onLogin: PropTypes.func,
-    /**
-     * A function called when the link to the reset password form is clicked.
-     */
-    onResetPassword: PropTypes.func.isRequired
-  };
-  static defaultProps = {
-    onLogin: () => {}
-  };
-  state = {
-    /**
-     * The user-entered username value.
-     * @type {String}
-     */
-    username: "",
-    /**
-     * The displayed error message.
-     * @type {String}
-     */
-    errorMessage: null,
-    /**
-     * The user-entered password value.
-     * @type {String}
-     */
-    password: "",
-    /**
-     * Is the register request pending?
-     * @type {Boolean}
-     */
-    pendingRequest: false
-  };
+export const LogInFormContainer = ({
+  id,
+  onSubmit,
+  onRegister,
+  onLogin,
+  onResetPassword
+}) => {
+  /**
+   * The user-entered username value.
+   * @type {String}
+   */
+  const [username, setUsername] = useState("");
+  /**
+   * The displayed error message.
+   * @type {String}
+   */
+  const [errorMessage, setErrorMessage] = useState(null);
+  /**
+   * The user-entered password value.
+   * @type {String}
+   */
+  const [password, setPassword] = useState("");
+  /**
+   * Is the register request pending?
+   * @type {Boolean}
+   */
+  const [pendingRequest, setPendingRequest] = useState(false);
 
   /**
    * Handles changes to the username field by storing it in state
-   * @param  {String} username The new username
+   * @param  {String} newUsername The new username
    */
-  handleUsernameChange = username => {
-    this.setState({
-      username,
-      errorMessage: null
-    });
+  const handleUsernameChange = newUsername => {
+    setUsername(newUsername);
+    setErrorMessage(null);
   };
 
   /**
    * Handles changes to the password field by storing it in state
-   * @param  {String} password The new password
+   * @param  {String} newPassword The new password
    */
-  handlePasswordChange = password => {
-    this.setState({
-      password,
-      errorMessage: null
-    });
+  const handlePasswordChange = newPassword => {
+    setPassword(newPassword);
+    setErrorMessage(null);
   };
 
   /**
    * Handles the log in form submission
    */
-  handleSubmit = () => {
-    this.setState({
-      pendingRequest: true
-    });
-    const cancelablePromise = makeCancelable(
-      this.props.onSubmit(this.state.username, this.state.password)
-    );
-    const promise = cancelablePromise.promise
-      .then(() => {
-        this.setState({
-          pendingRequest: false
-        });
-        this.props.onLogin();
-      })
-      .catch(error => {
-        if (error.isCanceled) {
-          return;
-        }
-        this.setState({
-          pendingRequest: false,
-          errorMessage: error
-        });
-      });
-    this.pendingPromises.push(cancelablePromise);
-    return promise;
+  const handleSubmit = async () => {
+    setPendingRequest(true);
+    try {
+      await onSubmit(username, password);
+      setPendingRequest(false);
+      onLogin();
+    } catch (error) {
+      setPendingRequest(false);
+      setErrorMessage(error);
+    }
   };
 
-  pendingPromises = [];
+  return (
+    <LogInForm
+      id={id}
+      errorMessage={errorMessage}
+      username={username}
+      password={password}
+      isDisabled={pendingRequest}
+      onUsernameChange={handleUsernameChange}
+      onPasswordChange={handlePasswordChange}
+      onSubmit={handleSubmit}
+      onRegister={onRegister}
+      onResetPassword={onResetPassword}
+    />
+  );
+};
 
-  componentWillUnmount() {
-    this.pendingPromises.forEach(promise => promise.cancel());
-  }
-
-  render() {
-    return (
-      <LogInForm
-        id={this.props.id}
-        errorMessage={this.state.errorMessage}
-        username={this.state.username}
-        password={this.state.password}
-        isDisabled={this.state.pendingRequest}
-        onUsernameChange={this.handleUsernameChange}
-        onPasswordChange={this.handlePasswordChange}
-        onSubmit={this.handleSubmit}
-        onRegister={this.props.onRegister}
-        onResetPassword={this.props.onResetPassword}
-      />
-    );
-  }
-}
+LogInFormContainer.propTypes = {
+  /**
+   * An identifieng string used to differentiate this from other instances
+   */
+  id: PropTypes.string.isRequired,
+  /**
+   * A function called when the log in form is submitted.
+   * Provided by redux.
+   */
+  onSubmit: PropTypes.func.isRequired,
+  /**
+   * A function called when the link to the register form is clicked.
+   */
+  onRegister: PropTypes.func.isRequired,
+  /** A function called when the user successfully logs in */
+  onLogin: PropTypes.func,
+  /**
+   * A function called when the link to the reset password form is clicked.
+   */
+  onResetPassword: PropTypes.func.isRequired
+};
 
 const mapStateToProps = () => ({});
 
@@ -135,7 +109,4 @@ const mapDispatchToProps = {
   onSubmit: LOG_IN
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LogInFormContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(LogInFormContainer);
