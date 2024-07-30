67df97d04da3eea6a15478ce5b82a4fc6a07f549
Spencer Watson <spencerwa@zillowgroup.com>
2024-07-30 10:52:08 -0700

    Update reset password form container

--- src/containers/__unit__/ResetPasswordFormContainer.test.js
+++ src/containers/__unit__/ResetPasswordFormContainer.test.js
@@ -1,10 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ResetPasswordForm from "../../components/ResetPasswordForm";
 import { ResetPasswordFormContainer } from "../ResetPasswordFormContainer";
+
+jest.mock("../../components/ResetPasswordForm", () => {
+  const MockResetPasswordForm = jest.fn();
+  return MockResetPasswordForm;
+});
 
 describe("ResetPasswordFormContainer", () => {
   let props;
   beforeEach(() => {
+    ResetPasswordForm.mockImplementation(() => (
+      <div data-testid="reset-password-form">Reset password form</div>
+    ));
     props = {
       id: "abc",
       onSubmit: jest.fn(),
@@ -12,76 +23,105 @@ describe("ResetPasswordFormContainer", () => {
     };
   });
   it("renders", () => {
-    expect.assertions(1);
-    const wrapper = shallow(<ResetPasswordFormContainer {...props} />);
-    expect(wrapper).toMatchSnapshot();
+    render(<ResetPasswordFormContainer {...props} />);
+    expect(screen.getByTestId("reset-password-form")).toHaveTextContent(
+      "Reset password form"
+    );
   });
   describe("#handleUsernameChange", () => {
-    it("triggers setState with the provided value", () => {
-      expect.assertions(2);
-      const subject = new ResetPasswordFormContainer(props);
-      subject.setState = jest.fn();
-      subject.handleUsernameChange("boogers");
-      expect(subject.setState.mock.calls).toHaveLength(1);
-      expect(subject.setState.mock.calls[0][0]).toEqual({
-        username: "boogers",
-        errorMessage: null
-      });
+    it("sets the username", async () => {
+      const MockResetPasswordForm = ({ onUsernameChange, username }) => (
+        <>
+          <div data-testid="username">{username}</div>
+          <div
+            data-testid="onUsernameChange"
+            onClick={() => onUsernameChange("boogers")}
+          />
+        </>
+      );
+      ResetPasswordForm.mockImplementation(MockResetPasswordForm);
+      render(<ResetPasswordFormContainer {...props} />);
+
+      expect(screen.getByTestId("username")).toHaveTextContent("");
+      await userEvent.click(screen.getByTestId("onUsernameChange"));
+      expect(screen.getByTestId("username")).toHaveTextContent("boogers");
     });
   });
   describe("#handleEmailChange", () => {
-    it("triggers setState with the provided value", () => {
-      expect.assertions(2);
-      const subject = new ResetPasswordFormContainer(props);
-      subject.setState = jest.fn();
-      subject.handleEmailChange("boogers");
-      expect(subject.setState.mock.calls).toHaveLength(1);
-      expect(subject.setState.mock.calls[0][0]).toEqual({
-        email: "boogers",
-        errorMessage: null
-      });
+    it("sets the email", async () => {
+      const MockResetPasswordForm = ({ onEmailChange, email }) => (
+        <>
+          <div data-testid="email">{email}</div>
+          <div
+            data-testid="onEmailChange"
+            onClick={() => onEmailChange("boogers")}
+          />
+        </>
+      );
+      ResetPasswordForm.mockImplementation(MockResetPasswordForm);
+      render(<ResetPasswordFormContainer {...props} />);
+
+      expect(screen.getByTestId("email")).toHaveTextContent("");
+      await userEvent.click(screen.getByTestId("onEmailChange"));
+      expect(screen.getByTestId("email")).toHaveTextContent("boogers");
     });
   });
   describe("#handleSubmit", () => {
-    it("maintains the pendingRequest state value", () => {
-      expect.assertions(4);
-      props.onSubmit.mockResolvedValue();
-      const subject = new ResetPasswordFormContainer(props);
-      subject.setState = jest.fn();
+    it("maintains the pendingRequest state value", async () => {
+      const MockResetPasswordForm = ({
+        isDisabled,
+        onEmailChange,
+        onUsernameChange,
+        onSubmit
+      }) => (
+        <>
+          <div data-testid="isDisabled">
+            {isDisabled ? "yes disabled" : "no disabled"}
+          </div>
+          <div
+            data-testid="onUsernameChange"
+            onClick={() => onUsernameChange("boogers")}
+          />
+          <div
+            data-testid="onEmailChange"
+            onClick={() => onEmailChange("boogers")}
+          />
+          <div data-testid="onSubmit" onClick={() => onSubmit()} />
+        </>
+      );
+      ResetPasswordForm.mockImplementation(MockResetPasswordForm);
+      props.onSubmit.mockImplementation(
+        () =>
+          new Promise(resolve => {
+            setTimeout(resolve, 10);
+          })
+      );
+      render(<ResetPasswordFormContainer {...props} />);
 
-      let promise = subject.handleSubmit();
-      expect(subject.setState.mock.calls).toHaveLength(1);
-      expect(subject.setState.mock.calls[0][0]).toEqual({
-        pendingRequest: true
-      });
-      promise = promise.then(() => {
-        expect(subject.setState.mock.calls).toHaveLength(2);
-        expect(subject.setState.mock.calls[1][0]).toEqual({
-          pendingRequest: false,
-          resetPasswordSent: true
-        });
-      });
-      return promise;
+      await userEvent.click(screen.getByTestId("onUsernameChange"));
+      await userEvent.click(screen.getByTestId("onEmailChange"));
+      expect(screen.getByTestId("isDisabled")).toHaveTextContent("no disabled");
+      await userEvent.click(screen.getByTestId("onSubmit"));
+      expect(screen.getByTestId("isDisabled")).toHaveTextContent(
+        "yes disabled"
+      );
+      await screen.findByText("no disabled");
+      expect(screen.getByTestId("isDisabled")).toHaveTextContent("no disabled");
     });
-    it("sets the error message if the call fails", () => {
-      expect.assertions(4);
+    it("sets the error message if the call fails", async () => {
+      const MockResetPasswordForm = ({ errorMessage, onSubmit }) => (
+        <>
+          <div data-testid="errorMessage">{errorMessage}</div>
+          <div data-testid="onSubmit" onClick={() => onSubmit()} />
+        </>
+      );
+      ResetPasswordForm.mockImplementation(MockResetPasswordForm);
       props.onSubmit.mockRejectedValue("fuck you");
-      const subject = new ResetPasswordFormContainer(props);
-      subject.setState = jest.fn();
+      render(<ResetPasswordFormContainer {...props} />);
 
-      let promise = subject.handleSubmit();
-      expect(subject.setState.mock.calls).toHaveLength(1);
-      expect(subject.setState.mock.calls[0][0]).toEqual({
-        pendingRequest: true
-      });
-      promise = promise.then(() => {
-        expect(subject.setState.mock.calls).toHaveLength(2);
-        expect(subject.setState.mock.calls[1][0]).toEqual({
-          pendingRequest: false,
-          errorMessage: "fuck you"
-        });
-      });
-      return promise;
+      expect(screen.getByTestId("errorMessage")).toHaveTextContent("");
+      await userEvent.click(screen.getByTestId("onSubmit"));
+      expect(screen.getByTestId("errorMessage")).toHaveTextContent("fuck you");
     });
   });
 });
